d2f45825390ce4516a6f740df684a8b94f5e1da7
Nikola <nikola_stojanovic1@yahoo.com>
2017-05-05 17:20:24 +0200

    Odradjena serijalizacija sva tri tipa entiteta.

--- HCI-projekat2/MainWindow.xaml.cs
+++ HCI-projekat2/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 using HCI_projekat2.Tabels;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace HCI_projekat2
 {
@@ -94,5 +96,152 @@ namespace HCI_projekat2
             var l = new ResourceTable();
             l.Show();
         }
+
+        private bool TipoveS()
+        {
+
+            FileStream stream = null;
+            BinaryFormatter bf = new BinaryFormatter();
+            bool retVal = false;
+            try
+            {
+                stream = File.Open("resursi.nkvd", FileMode.OpenOrCreate);
+                bf.Serialize(stream, _resursi);
+            }
+            catch
+            {
+                //nista
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Dispose();
+                    retVal = true;
+                }
+            }
+            return retVal;
+        }
+
+        private bool EtiketeS()
+        {
+
+            FileStream stream = null;
+            BinaryFormatter bf = new BinaryFormatter();
+            bool retVal = false;
+            try
+            {
+                stream = File.Open("etikete.nkvd", FileMode.OpenOrCreate);
+                bf.Serialize(stream, _etikete);
+            }
+            catch
+            {
+                //nista
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Dispose();
+                    retVal = true;
+                }
+            }
+            return retVal;
+        }
+
+        private bool ResurseS()
+        {
+
+            FileStream stream = null;
+            BinaryFormatter bf = new BinaryFormatter();
+            bool retVal = false;
+            try
+            {
+                stream = File.Open("resursi.nkvd", FileMode.OpenOrCreate);
+                bf.Serialize(stream, _resursi);
+            }
+            catch
+            {
+                //nista
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Dispose();
+                    retVal = true;
+                }
+            }
+            return retVal;
+        }
+
+        private void ResurseS_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResurseS())
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Resursi su uspešno sačuvani.", "Operacija uspešna", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            else
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Došlo je do greške prilikom čuvanja!", "Operacija neuspešna", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void TipoveS_Click(object sender, RoutedEventArgs e)
+        {
+            if (TipoveS())
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Tipovi su uspešno sačuvani.", "Operacija uspešna", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            else
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Došlo je do greške prilikom čuvanja!", "Operacija neuspešna", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void EtiketeS_Click(object sender, RoutedEventArgs e)
+        {
+            if (EtiketeS())
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Etikete su uspešno sačuvane.", "Operacija uspešna", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            else
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Došlo je do greške prilikom čuvanja!", "Operacija neuspešna", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Sacuvaj_Sve_Click(object sender, RoutedEventArgs e)
+        {
+            if (EtiketeS() && ResurseS() && TipoveS())
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Podaci su uspešno sačuvani.", "Operacija uspešna", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            else
+            {
+                MessageBoxResult msg;
+                msg = MessageBox.Show(this, "Došlo je do greške prilikom čuvanja!", "Operacija neuspešna", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult msb = MessageBox.Show("Želite li da sačuvate unete izmene?", "Čuvanje pre zatvaranja", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (msb == MessageBoxResult.Yes)
+            {
+                EtiketeS();
+                TipoveS();
+                ResurseS();
+            }
+            base.OnClosing(e);
+        }
     }
 }
+
