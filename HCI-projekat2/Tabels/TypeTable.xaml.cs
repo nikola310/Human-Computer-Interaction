13080d5d765591c1e558248585fac06cc7a21ea8
Nikola <nikola_stojanovic1@yahoo.com>
2017-05-06 10:47:59 +0200

    Izmene u tabeli tipova.

--- HCI-projekat2/Tabels/TypeTable.xaml.cs
+++ HCI-projekat2/Tabels/TypeTable.xaml.cs
@@ -2,6 +2,9 @@
 using System.Windows;
 using HCI_projekat2.Model;
 using static HCI_projekat2.MainWindow;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+using System;
 
 namespace HCI_projekat2.Tabels
 {
@@ -23,6 +26,46 @@ namespace HCI_projekat2.Tabels
                 tipovi.Add(s);
             }
             DataContext = this;
+        }
+
+        private void Odustani_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Izmeni_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Obrisi_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(this, "Jeste li sigurni da želite obrisati selektovani tip?", "Potvrda brisanja tipa", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                TypeModel model = (TypeModel)dgrMain.SelectedItem;
+                Tipovi.Remove(model.ID);
+                MessageBox.Show(this, "Tip resursa je obrisan.", "Operacija uspešna", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dijalog = new OpenFileDialog();
+            TypeModel model = (TypeModel)dgrMain.SelectedItem;
+            dijalog.Filter = "Slike (*.jpeg, *.jpg, *.png, *.ico)|*.jpeg; *.jpg; *.png; *.ico";
+            bool? retVal = dijalog.ShowDialog();
+            if (retVal == true)
+            {
+                string fajl = dijalog.FileName;
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new System.Uri(fajl, UriKind.Absolute);
+                img.EndInit();
+                model.IconPath = fajl;
+                Ikona.Source = img;
+            }
         }
     }
 }
